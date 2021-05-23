2bd17c9817daeceb8e70ef52956cf8d8ca662bd6
Dipanjan Jayswal <iamdipanjanjayswal@gmail.com>
2021-05-23 15:49:54 +0530

    added axios to fetch data from unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
+import axios from "axios";
 import React from "react";
 
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID CVh6AJFiZP7XaOTRszGHWSlFbSDq_fHfJe_ZAtpEmls",
+      },
+    });
   }
 
   render() {
